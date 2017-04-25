daf837e3d44fb5fb62e4999d60ca1b8a6d59db5d
Jordan Harrod <jordanharrod@dhcp-hol-183.eduroam.cornell.edu>
2017-04-25 16:08:15 -0400

    basic layout things

--- MotionBasedSecurityKey/ViewController.swift
+++ MotionBasedSecurityKey/ViewController.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+//import CMLogItem
 
 class ViewController: UIViewController {
+
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +23,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //MARK: Actions
+    
+    @IBAction func startRecordingPassword(_ sender: UIButton) {
+        
+        //create Gryoscope object
+        //measure changes in acceleration/position
+        //save data to a location
+        //change string label to "Done!" once timed out
+        //move to next view controller
+        
+    }
+    
+    //func inputPassword {
+    // record new password 
+    // compare to set password 
+    // move to an accept screen if similar enough
+    // reject with vibration otherwise
 
 }
 
