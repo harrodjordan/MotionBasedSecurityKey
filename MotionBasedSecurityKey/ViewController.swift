f19a0259ff7676eff36d8210da214e2528c327cc
Jordan Harrod <jordanharrod@dhcp-hol-183.eduroam.cornell.edu>
2017-04-25 15:30:49 -0400

    Initial Commit

---
+++ MotionBasedSecurityKey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MotionBasedSecurityKey
+//
+//  Created by Jordan Harrod on 4/25/17.
+//  Copyright © 2017 Jordan Harrod. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
